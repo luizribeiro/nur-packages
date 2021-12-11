d8851cc2bee1ff282212d3a6e32ab31232120243
Luiz Ribeiro <luizribeiro@gmail.com>
2021-12-10 19:28:35 -0600

    [ps5-wake] Limit only to Linux

--- pkgs/ps5-wake/default.nix
+++ pkgs/ps5-wake/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, pkgs, ... }:
+{ stdenv, pkgs, lib, ... }:
 
 stdenv.mkDerivation {
   name = "ps5-wake";
@@ -8,6 +8,10 @@ stdenv.mkDerivation {
     repo = "ps5-wake";
     rev = "7699c937c834e49a87d72870e212b7999bffd8b6";
     sha256 = "sha256-N7uUpMB8g3g6shTjw2MmvzsoCZ+beEIUi1zpafIj7+U=";
+  };
+
+  meta = with lib; {
+    platforms = platforms.linux;
   };
 
   buildInputs = [
