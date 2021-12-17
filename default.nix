09d151ddcd31bd7c708ac6dafc6d1867128f48fe
Luiz Ribeiro <luizribeiro@gmail.com>
2021-12-16 23:16:10 -0600

    [alfred] Created a package for Alfred.app

--- default.nix
+++ default.nix
@@ -5,6 +5,7 @@
   modules = import ./modules;
   overlays = import ./overlays;
 
+  alfred = pkgs.callPackage ./pkgs/alfred { };
   dokku-client = pkgs.callPackage ./pkgs/dokku-client { inherit pkgs; };
   influx-cli = pkgs.callPackage ./pkgs/influx-cli { };
   ps5-wake = pkgs.callPackage ./pkgs/ps5-wake { inherit pkgs; };
---
+++ pkgs/alfred/default.nix
@@ -0,0 +1,29 @@
+{ stdenv, lib, fetchurl, undmg }:
+
+with lib;
+
+stdenv.mkDerivation rec {
+  pname = "alfred";
+  version = "4.6.1";
+
+  # The disk image contains the .app and a symlink to /Applications.
+  sourceRoot = "Alfred 4.app";
+
+  src = fetchurl {
+    url = "https://cachefly.alfredapp.com/Alfred_4.6.1_1274.dmg";
+    sha256 = "sha256-KFGm2gDorYW7AAkxodnb2gDSdALU47fI+9d9iVawCbM=";
+  };
+
+  buildInputs = [ undmg ];
+  installPhase = ''
+    mkdir -p "$out/Applications/Alfred 4.app"
+    cp -R . "$out/Applications/Alfred 4.app"
+  '';
+
+  meta = with lib; {
+    description = "Application launcher and productivity software";
+    homepage = "https://www.alfredapp.com/";
+    license = licenses.unfree;
+    platforms = platforms.darwin;
+  };
+}
