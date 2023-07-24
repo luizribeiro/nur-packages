83e9cb4508d87f2ee1a6aa91caae693396038cc0
Luiz Ribeiro <luizribeiro@gmail.com>
2023-07-23 20:26:47 -0400

    [bambu-studio] added desktop icon

--- pkgs/bambu-studio/default.nix
+++ pkgs/bambu-studio/default.nix
@@ -2,7 +2,6 @@
 
 let
   name = "bambu-studio";
-  version = "01.07.00.65";
   src = pkgs.fetchurl {
     url = "https://github.com/bambulab/BambuStudio/releases/download/v01.07.00.65/BambuStudio_fedora_v01.07.00.65-20230713174938.AppImage";
     hash = "sha256-t+ouo9YCwmOBF58uY0mD0l9GCygRc3uD+TP/OVm0GAg=";
@@ -10,11 +9,12 @@ let
   desktopItem = pkgs.makeDesktopItem {
     inherit name;
     exec = name;
-    #icon = "Logic";
+    icon = "BambuStudio";
     comment = "Software for Bambu Lab 3D printers";
     desktopName = "Bambu Studio";
     categories = [ "Development" ];
   };
+  appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
 in
 pkgs.appimageTools.wrapType2 {
   inherit name src;
@@ -25,6 +25,12 @@ pkgs.appimageTools.wrapType2 {
     webkitgtk
     zstd
   ]);
+
+  extraInstallCommands = ''
+    mkdir -p $out/share/pixmaps
+    ln -s ${desktopItem}/share/applications $out/share/
+    cp ${appimageContents}/usr/share/icons/hicolor/192x192/apps/BambuStudio.png $out/share/pixmaps/BambuStudio.png
+  '';
 
   meta = with lib; {
     homepage = "https://github.com/bambulab/BambuStudio/";
