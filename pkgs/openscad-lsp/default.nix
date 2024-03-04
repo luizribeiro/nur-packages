e1e86667198d856ab1e7088302b177d9b9950516
Luiz Ribeiro <luizribeiro@gmail.com>
2024-03-04 09:53:54 -0500

    ran uptix

--- pkgs/openscad-lsp/default.nix
+++ pkgs/openscad-lsp/default.nix
@@ -12,6 +12,6 @@ rustPlatform.buildRustPackage {
     description = "A LSP server for OpenSCAD";
     homepage = "https://github.com/Leathong/openscad-LSP";
     license = pkgs.lib.licenses.asl20;
-    platforms = [ "x86_64-linux" "x86_64-darwin" ];
+    platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
   };
 }
